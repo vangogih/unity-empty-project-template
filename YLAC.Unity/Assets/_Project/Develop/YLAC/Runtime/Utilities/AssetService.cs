875469f2ffeddde4c1ffbf5199bf7ccd89041f20
vangogih <vangogih@yandex.ru>
2025-07-21 20:42:43 +0200

    resource manager dependency for runtime

--- YLAC.Unity/Assets/_Project/Develop/YLAC/Runtime/Utilities/AssetService.cs
+++ YLAC.Unity/Assets/_Project/Develop/YLAC/Runtime/Utilities/AssetService.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace YLAC.Runtime.Utilities
 {
     public static class AssetService
     {
         public static Resources R { get; } = new();
+        public static Addressables Add { get; } = new();
     }
 
     public sealed class Resources
@@ -13,6 +17,23 @@ namespace YLAC.Runtime.Utilities
         {
             var result = UnityEngine.Resources.Load<T>(path);
             return result;
+        }
+    }
+
+    public sealed class Addressables
+    {
+        private Dictionary<string, AsyncOperationHandle> _toRelease = new Dictionary<string, AsyncOperationHandle>();
+        public UniTask<T> LoadAsync<T>(string key)
+        {
+            var op = UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<T>(key);
+            _toRelease.Add(key, op);
+            var uniTask = op.ToUniTask();
+            return uniTask;
+        }
+
+        public void Release(string key)
+        {
+            UnityEngine.AddressableAssets.Addressables.Release(_toRelease[key]);
         }
     }
 }
